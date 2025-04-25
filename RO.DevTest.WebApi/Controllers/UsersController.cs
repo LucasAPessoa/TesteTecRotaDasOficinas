fb2abebf91d4f6162369f2e620edff0c7d993a6d
Lucas <lucasapessoa@outlook.com>
2025-04-25 13:32:19 -0300

    feat: adiciona UsersController para as rotas implementadas

--- RO.DevTest.WebApi/Controllers/UsersController.cs
+++ RO.DevTest.WebApi/Controllers/UsersController.cs
@@ -1,21 +1,80 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using NSwag.Annotations;
 using RO.DevTest.Application.Features.User.Commands.CreateUserCommand;
+using RO.DevTest.Application.Features.User.Commands.DeleteUserCommand;
+using RO.DevTest.Application.Features.User.Commands.UpdateUserCommand;
+using RO.DevTest.Application.Features.User.Queries.GetAllUsersQuery;
+using RO.DevTest.Application.Features.User.Queries.GetUserByEmailQuery;
+using RO.DevTest.Application.Features.User.Queries.GetUserByIdQuery;
 
 namespace RO.DevTest.WebApi.Controllers;
 
 [Route("api/user")]
+[ApiController]
 [OpenApiTags("Users")]
-public class UsersController(IMediator mediator) : Controller {
+public class UsersController(IMediator mediator) : Controller
+{
     private readonly IMediator _mediator = mediator;
 
     [HttpPost]
     [ProducesResponseType(typeof(CreateUserResult), StatusCodes.Status201Created)]
     [ProducesResponseType(typeof(CreateUserResult), StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> CreateUser(CreateUserCommand request) {
+    public async Task<IActionResult> CreateUser([FromBody] CreateUserCommand request)
+    {
         CreateUserResult response = await _mediator.Send(request);
         return Created(HttpContext.Request.GetDisplayUrl(), response);
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(CreateUserResult), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetUsers([FromQuery] GetAllUsersQuery request)
+    {
+        var response = await _mediator.Send(request);
+        return Ok(response);
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(CreateUserResult), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetUserById([FromRoute] string id)
+    {
+        var response = await _mediator.Send(new GetUserByIdQuery(id));
+        return Ok(response);
+    }
+
+    [HttpGet("email/{email}")]
+    [ProducesResponseType(typeof(CreateUserResult), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetUserByEmail([FromRoute] string email)
+    {
+        var response = await _mediator.Send(new GetUserByEmailQuery(email));
+        return Ok(response);
+    }
+
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(typeof(CreateUserResult), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> DeleteUser([FromRoute] string id)
+    {
+        var response = await _mediator.Send(new DeleteUserCommand(id));
+        return Ok(response);
+    }
+
+    [HttpPatch("{id}")]
+    [ProducesResponseType(typeof(CreateUserResult), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> UpdateUser([FromRoute] string id, [FromBody] UpdateUserCommand request)
+    {
+        if (id != request.Id)
+            return BadRequest("O ID do usuário não confere com o ID da requisição");
+        var response = await _mediator.Send(request);
+        return Ok(response);
     }
 }
