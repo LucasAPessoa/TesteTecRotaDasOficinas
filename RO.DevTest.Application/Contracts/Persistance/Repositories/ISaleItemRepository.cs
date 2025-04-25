f67f99f8aee7882613d189fdaee905d3ecb5e9f4
Lucas <lucasapessoa@outlook.com>
2025-04-25 12:58:11 -0300

    feat: adiciona ISaleItemRepository com os métodos necessários

---
+++ RO.DevTest.Application/Contracts/Persistance/Repositories/ISaleItemRepository.cs
@@ -0,0 +1,15 @@
+﻿using RO.DevTest.Domain.Entities;
+
+namespace RO.DevTest.Application.Contracts.Persistance.Repositories
+{
+    public interface ISaleItemRepository
+    {
+        Task<SaleItem> GetByIdAsync(Guid id);
+        Task<SaleItem> GetByProductIdAsync(Guid productId);
+        Task<List<SaleItem>> GetAllAsync();
+        Task UpdateAsync(SaleItem saleItem);
+        Task DeleteAsync(Guid id);
+        Task DeleteManyAsync(IEnumerable<Guid> ids);
+        Task<SaleItem> CreateAsync(SaleItem saleItem);
+    }
+}
