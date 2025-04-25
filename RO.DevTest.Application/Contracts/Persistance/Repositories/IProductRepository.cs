19f22bede1658a8f9ddf3110715724b8be6a4967
Lucas <lucasapessoa@outlook.com>
2025-04-25 12:34:26 -0300

    feat: adiciona o IProductRepository com os métodos necessários

---
+++ RO.DevTest.Application/Contracts/Persistance/Repositories/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using RO.DevTest.Domain.Entities;
+
+namespace RO.DevTest.Application.Contracts.Persistance.Repositories
+{
+    public interface IProductRepository 
+    {
+        Task<Product> GetByIdAsync(Guid id);
+        Task<Product?> GetByNameAsync(string name);
+        IQueryable<Product> GetAllAsQueryable();
+        Task<Product> CreateAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task DeleteAsync(Guid id);
+    }
+    
+}
+
