c3941c81baee03d98b78399db29ca6d2bb2ab533
Lucas <lucasapessoa@outlook.com>
2025-04-25 12:27:12 -0300

    feat: adiciona a entidade SaleItem

---
+++ RO.DevTest.Domain/Entities/SaleItem.cs
@@ -0,0 +1,12 @@
+﻿using RO.DevTest.Domain.Abstract;
+
+namespace RO.DevTest.Domain.Entities
+{
+    public class SaleItem : BaseEntity
+    {
+        public Product Product { get; set; } = new Product();
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal TotalPrice => Product.Price * Quantity;
+    }
+}
