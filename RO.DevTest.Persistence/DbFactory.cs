33462eb4b27486253b02424eaf296f373edc1c31
Lucas <lucasapessoa@outlook.com>
2025-04-25 13:10:43 -0300

    chore: adiciona DbFactory para suporte a migrations

---
+++ RO.DevTest.Persistence/DbFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace RO.DevTest.Persistence
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DefaultContext>
+    {
+        public DefaultContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DefaultContext>();
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "..", "RO.DevTest.WebApi"))
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new DefaultContext(optionsBuilder.Options);
+        }
+    }
+}
