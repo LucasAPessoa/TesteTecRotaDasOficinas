842d99e9f0d1d4ba03e89f681ae19196f2a185bc
Lucas <lucasapessoa@outlook.com>
2025-04-25 12:26:25 -0300

    feat: adiciona a entidade Product

---
+++ RO.DevTest.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RO.DevTest.Domain.Abstract;
+
+namespace RO.DevTest.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; } = 0.0m;
+
+    }
+}
