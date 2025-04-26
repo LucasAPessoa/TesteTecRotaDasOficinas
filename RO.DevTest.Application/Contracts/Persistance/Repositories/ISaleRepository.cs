f588249292f349e656855b48fe87b6ac26a6642a
Lucas <lucasapessoa@outlook.com>
2025-04-26 10:38:36 -0300

    feat: adiciona a interface de SaleRepository com todo o CRUD implementado

---
+++ RO.DevTest.Application/Contracts/Persistance/Repositories/ISaleRepository.cs
@@ -0,0 +1,16 @@
+﻿using RO.DevTest.Domain.Entities;
+
+namespace RO.DevTest.Application.Contracts.Persistance.Repositories
+{
+    public interface ISaleRepository 
+    {
+        IQueryable<Sale> GetAllAsQueryable();
+        Task<Sale> GetByIdAsync(Guid id);
+        Task<Sale> CreateAsync(Sale sale);
+        Task<Sale> UpdateAsync(Sale sale);
+        Task DeleteAsync(Guid Id);
+        Task SaveChangesAsync();
+
+
+    }
+}
