772ef05ed0e416eb4b75b9798ce3f909b429f398
Lucas <lucasapessoa@outlook.com>
2025-04-26 10:30:33 -0300

    feat: adiciona controller com autenticação implementada

--- RO.DevTest.WebApi/Controllers/AuthController.cs
+++ RO.DevTest.WebApi/Controllers/AuthController.cs
@@ -1,13 +1,46 @@
-﻿using MediatR;
-using Microsoft.AspNetCore.Mvc;
-using NSwag.Annotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using RO.DevTest.Application.Common.Interfaces;
+using Microsoft.AspNetCore.Identity.Data;
+using RO.DevTest.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
 
 namespace RO.DevTest.WebApi.Controllers;
 
+[ApiController]
 [Route("api/auth")]
-[OpenApiTags("Auth")]
-public class AuthController(IMediator mediator) : Controller {
-    private readonly IMediator _mediator = mediator;
+public class AuthController : ControllerBase
+{
+    private readonly UserManager<User> _userManager;
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
 
-    ///[TODO] - CREATE LOGIN HANDLER HERE 
+    public AuthController(UserManager<User> userManager, IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _userManager = userManager;
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+
+    [HttpPost("login")]
+    [AllowAnonymous]
+    public async Task<IActionResult> Login([FromBody] LoginRequest request)
+    {
+        var user = await _userManager.FindByEmailAsync(request.Email);
+
+        if (user == null)
+        {
+            Console.WriteLine("Usuário não encontrado.");
+            return Unauthorized(new { Message = "Email ou senha inválidos." });
+        }
+
+        var isPasswordValid = await _userManager.CheckPasswordAsync(user, request.Password);
+        Console.WriteLine($"Usuário: {user.Email}, Senha Válida: {isPasswordValid}");
+
+        if (!isPasswordValid)
+        {
+            return Unauthorized(new { Message = "Email ou senha inválidos." });
+        }
+
+        var token = _jwtTokenGenerator.GenerateToken(user);
+        return Ok(new { Token = token });
+    }
 }
