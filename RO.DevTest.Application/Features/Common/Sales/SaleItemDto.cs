f9f4e51a8dbed50b692eb539aa0605a0da3d9f68
Lucas <lucasapessoa@outlook.com>
2025-04-26 10:46:55 -0300

    feat: adiciona SaleItemDto para representação de itens de venda

---
+++ RO.DevTest.Application/Features/Common/Sales/SaleItemDto.cs
@@ -0,0 +1,11 @@
+﻿
+using RO.DevTest.Domain.Abstract;
+
+namespace RO.DevTest.Application.Features.Sales.Common;
+
+public class SaleItemDto : BaseEntity
+{
+    public Guid ProductId { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+}
