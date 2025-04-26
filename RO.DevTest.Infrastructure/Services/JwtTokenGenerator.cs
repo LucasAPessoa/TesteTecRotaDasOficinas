dab185f688023f9b6e1192c8ca1ea6b5f56b3e9b
Lucas <lucasapessoa@outlook.com>
2025-04-26 10:26:54 -0300

    feat: adiciona serviço de geração de Tokens Jwt

---
+++ RO.DevTest.Infrastructure/Services/JwtTokenGenerator.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using RO.DevTest.Application.Common.Interfaces;
+using RO.DevTest.Infrastructure.Settings;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UserEntity = RO.DevTest.Domain.Entities.User;
+
+public class JwtTokenGenerator : IJwtTokenGenerator
+{
+    private readonly JwtSettings _jwtSettings;
+
+    public JwtTokenGenerator(JwtSettings jwtSettings)
+    {
+        _jwtSettings = jwtSettings;
+    }
+
+    public string GenerateToken(UserEntity user)
+    {
+        var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key));
+        var credentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var token = new JwtSecurityToken(
+            issuer: _jwtSettings.Issuer,
+            audience: _jwtSettings.Audience,
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(_jwtSettings.ExpiryMinutes),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
