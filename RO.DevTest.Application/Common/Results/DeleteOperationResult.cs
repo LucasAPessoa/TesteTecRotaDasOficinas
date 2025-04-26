10eef154db3523f2c455d38eca0d35b749b2fb57
Lucas <lucasapessoa@outlook.com>
2025-04-26 10:15:56 -0300

    feat: adiciona um modelo para a operação de Delete

---
+++ RO.DevTest.Application/Common/Results/DeleteOperationResult.cs
@@ -0,0 +1,27 @@
+﻿namespace RO.DevTest.Application.Common.Results
+{
+    public class DeleteOperationResult
+    {
+        public bool Succeeded { get; set; }
+        public List<string> Errors { get; set; } = new();
+        public string Message { get; set; } = string.Empty;
+
+        public static DeleteOperationResult Success()
+        {
+            return new DeleteOperationResult { 
+                Succeeded = true,
+
+            };
+
+        }
+
+        public static DeleteOperationResult Failure(params string[] errors)
+        {
+            return new DeleteOperationResult
+            {
+                Succeeded = false,
+                Errors = errors.ToList(),
+            };
+        }
+    }
+}
